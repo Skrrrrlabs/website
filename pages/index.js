b4097acc4066fb6bc30d46a47bea3adc7165d7a8
Skrrrrlabs <Skrrrrlabs@users.noreply.github.com>
2026-07-11 13:22:28 +0900

    Refine language-specific typography

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
   const t = copy[lang];
 
   return (
-    <div className={styles.site}>
+    <div className={styles.site} lang={lang}>
       <Head>
         <title>{t.metaTitle}</title>
         <meta name="description" content={t.metaDescription} />
